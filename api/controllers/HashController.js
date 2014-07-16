2f44479cac16fbb636156f082d29940fefbeed8c
Theadd <pantallazo@gmail.com>
2014-07-17 00:47:09 +0200

    search using text index

--- api/controllers/HashController.js
+++ api/controllers/HashController.js
@@ -52,11 +52,11 @@ module.exports = {
 
   },
 
-  /**
-   * HashController.search()
-   * @example: http://localhost:1337/hash/search?query=2014%20720%20|%201080&category=movie%20|%20tv
+  /** DEPRECATED
+   * HashController.raw_search()
+   * @example: http://localhost:1337/hash/raw_search?query=2014%20720%20|%201080&category=movie%20|%20tv
    */
-  search: function (req, res) {
+  raw_search: function (req, res) {
     var i,
       query = Hash.find(),
       queries = QueryHelpers.getWaterlineQueryFromString(req.param('query'), 'title'),
@@ -75,55 +75,53 @@ module.exports = {
       if (err) return res.send(err,500);
 
       if (typeof req.param('define') !== "undefined" && req.param('define').length) {
-
-        var extend = require('util')._extend
-        var safeEval = (function(input) {
-          'use strict'
-          return eval(input)
-        })
-
-        for (i = 0; i < hashes.length; ++i) {
-          var item = extend({}, hashes[i])
-
-          var snippet = req.param('define')
-          snippet = snippet.replace(/\$item\.uuid/g, hashes[i]['uuid'])
-            .replace(/\$item\.cache/g, hashes[i]['cache'] || '')
-            .replace(/\$item\.title/g, hashes[i]['title'] || '')
-            .replace(/\$item\.added/g, hashes[i]['added'] || '')
-            .replace(/\$item\.updatedAt/g, hashes[i]['updatedAt'] || '')
-            .replace(/\$item\.createdAt/g, hashes[i]['createdAt'] || '')
-            .replace(/\$item\.rate/g, hashes[i]['rate'] || '0')
-            .replace(/\$item\.media/g, JSON.stringify(hashes[i]['media']) || '{}')
-            .replace(/\$item\.status/g, hashes[i]['status'] || '0')
-            .replace(/\$item\.leechers/g, hashes[i]['leechers'] || '0')
-            .replace(/\$item\.seeders/g, hashes[i]['seeders'] || '0')
-            .replace(/\$item\.size/g, hashes[i]['size'] || '0')
-            .replace(/\$item\.downloaded/g, hashes[i]['downloaded'] || 'false')
-            .replace(/\$item\.files/g, hashes[i]['files'] || '0')
-            .replace(/\$item\.trackers/g, JSON.stringify(hashes[i]['trackers']) || '0')
-            .replace(/\$item\.source/g, hashes[i]['source'] || '')
-            .replace(/\$item\.category/g, hashes[i]['category'] || '')
-
-          var aux = "var json = " + snippet + "; json"
-          try {
-            var evaluated = safeEval(aux)
-
-            extend(item, evaluated)
-            hashes[i] = item
-          } catch (e) {
-            console.log(e);
-            hashes[i] = { 'error': true, 'snippet': snippet, 'eval': aux }
-          }
-          /*
-           {link:%20(%27$item.cache%27.length)%20?%20%27http://$item.cache/torrent/$item.uuid.torrent%27%20:%20%27magnet:?xt=urn:btih:%27%20%2B%20%27$item.uuid%27.toLowerCase()%20%2B%20%27%26dn=%27%20%2B%20encodeURIComponent(%27$item.title%27)%20}
-           {link: ('$item.cache'.length) ? 'http://$item.cache/torrent/$item.uuid.torrent' : 'magnet:?xt=urn:btih:' + '$item.uuid'.toLowerCase() + '&dn=' + encodeURI('$item.title') }
-           text = text.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\n/g, '<br/>');
-           */
-        }
+        hashes = applyResultsModifier(hashes, req.param('define'))
+        //{link:%20(%27$item.cache%27.length)%20?%20%27http://$item.cache/torrent/$item.uuid.torrent%27%20:%20%27magnet:?xt=urn:btih:%27%20%2B%20%27$item.uuid%27.toLowerCase()%20%2B%20%27%26dn=%27%20%2B%20encodeURIComponent(%27$item.title%27)%20}
+        //{link: ('$item.cache'.length) ? 'http://$item.cache/torrent/$item.uuid.torrent' : 'magnet:?xt=urn:btih:' + '$item.uuid'.toLowerCase() + '&dn=' + encodeURI('$item.title') }
       }
 
       res.json(hashes);
 
+    });
+  },
+  /**
+   * HashController.search()
+   * @example: http://localhost:1337/hash/search?title=2014 720p -subs -screener -cam
+   */
+  search: function (req, res) {
+    Hash.native(function(err, collection) {
+      var title = req.param('title')
+
+      collection.find(
+      {
+        $text: {
+          $search: title
+        }
+      }, {
+        score: {
+          $meta: "textScore"
+        }
+      }, {
+        explain: Boolean(req.param('explain'))
+      })
+      .sort(
+      {
+        score: {
+          $meta: "textScore"
+        }
+      })
+      .limit(20)
+      .toArray(function(err, results){
+        if (err) return res.send(err,500);
+
+        if (typeof req.param('define') !== "undefined" && req.param('define').length) {
+          results = applyResultsModifier(results, req.param('define'))
+          //{link:%20(%27$item.cache%27.length)%20?%20%27http://$item.cache/torrent/$item.uuid.torrent%27%20:%20%27magnet:?xt=urn:btih:%27%20%2B%20%27$item.uuid%27.toLowerCase()%20%2B%20%27%26dn=%27%20%2B%20encodeURIComponent(%27$item.title%27)%20}
+          //{link: ('$item.cache'.length) ? 'http://$item.cache/torrent/$item.uuid.torrent' : 'magnet:?xt=urn:btih:' + '$item.uuid'.toLowerCase() + '&dn=' + encodeURI('$item.title') }
+        }
+
+        res.json(results);
+      });
     });
   },
 
@@ -152,3 +150,48 @@ module.exports = {
   }
 
 };
+
+function applyResultsModifier (hashes, modifier) {
+  var extend = require('util')._extend
+  var safeEval = (function(input) {
+    'use strict'
+    return eval(input)
+  })
+
+  for (var i = 0; i < hashes.length; ++i) {
+    var item = extend({}, hashes[i])
+
+    var snippet = modifier
+    snippet = snippet.replace(/\$item\.uuid/g, hashes[i]['uuid'])
+      .replace(/\$item\.cache/g, hashes[i]['cache'] || '')
+      .replace(/\$item\.title/g, hashes[i]['title'] || '')
+      .replace(/\$item\.added/g, hashes[i]['added'] || '')
+      .replace(/\$item\.updatedAt/g, hashes[i]['updatedAt'] || '')
+      .replace(/\$item\.createdAt/g, hashes[i]['createdAt'] || '')
+      .replace(/\$item\.rate/g, hashes[i]['rate'] || '0')
+      .replace(/\$item\.media/g, JSON.stringify(hashes[i]['media']) || '{}')
+      .replace(/\$item\.status/g, hashes[i]['status'] || '0')
+      .replace(/\$item\.leechers/g, hashes[i]['leechers'] || '0')
+      .replace(/\$item\.seeders/g, hashes[i]['seeders'] || '0')
+      .replace(/\$item\.size/g, hashes[i]['size'] || '0')
+      .replace(/\$item\.downloaded/g, hashes[i]['downloaded'] || 'false')
+      .replace(/\$item\.files/g, hashes[i]['files'] || '0')
+      .replace(/\$item\.trackers/g, JSON.stringify(hashes[i]['trackers']) || '0')
+      .replace(/\$item\.source/g, hashes[i]['source'] || '')
+      .replace(/\$item\.category/g, hashes[i]['category'] || '')
+
+    var aux = "var json = " + snippet + "; json"
+    try {
+      var evaluated = safeEval(aux)
+
+      extend(item, evaluated)
+      hashes[i] = item
+    } catch (e) {
+      console.log(e)
+      hashes[i] = { 'error': true, 'snippet': snippet, 'eval': aux }
+    }
+
+  }
+
+  return hashes
+}
