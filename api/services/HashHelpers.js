225793edffa321e19e1047dff5a8350a7157118c
Theadd <pantallazo@gmail.com>
2014-09-11 01:45:46 +0200

    Refactoring

--- api/services/HashHelpers.js
+++ api/services/HashHelpers.js
@@ -2,47 +2,54 @@
  * Created by Theadd on 11/09/2014.
  */
 
+var isMostUpdated = exports.isMostUpdated = function (existing, received) {
+  var mostUpdated = false
+
+  if (typeof existing.peersUpdatedAt !== "undefined") {
+    if (typeof received.peersUpdatedAt !== "undefined") {
+      mostUpdated = (existing.peersUpdatedAt < received.peersUpdatedAt)
+    }
+  } else {
+    if (typeof received.peersUpdatedAt !== "undefined") {
+      mostUpdated = true
+    } else {
+      mostUpdated = (existing.updatedAt < received.updatedAt)
+    }
+  }
+
+  return mostUpdated
+}
+
 exports.merge = function (item) {
   Hash.find()
     .where({ uuid: item.uuid })
     .exec(function (err, entries) {
       if (!err && entries.length) {
-        if (entries[0].rate != item.rate) {
-          if (entries[0].updatedAt > item.updatedAt) {
-            //do not update media info and status
-            Hash.update({ uuid: item.uuid }, {
-              seeders: item.seeders,
-              leechers: item.leechers,
-              updatedAt: entries[0].updatedAt,
-              peersUpdatedAt: item.peersUpdatedAt,
-              updatedBy: item.updatedBy
-            }, function (err, hashes) {
-              //updated
-            })
+        if (isMostUpdated(entries[0], item)) {
+          if (CommandLineHelpers.config.removedead && item.seeders == 0) {
+            //TODO: REMOVE DEAD
+            console.log("######## DEAD FOUND: " + item.uuid)
+            console.log(item)
+            console.log("\n")
           } else {
-            //update media info too
+            var recentUpdatedAt = (entries[0].updatedAt < item.updatedAt),
+              rateMatch = (entries[0].rate == item.rate)
+
+            console.log("\t" + item.uuid + " is being updated.")
             Hash.update({ uuid: item.uuid }, {
               seeders: item.seeders,
               leechers: item.leechers,
-              updatedAt: item.updatedAt,
+              updatedAt: (recentUpdatedAt) ? item.updatedAt : entries[0].updatedAt,
               peersUpdatedAt: item.peersUpdatedAt,
               updatedBy: item.updatedBy,
-              rate: item.rate,
-              media: item.media
+              rate: (!rateMatch && recentUpdatedAt) ? item.rate : entries[0].rate,
+              media: (!rateMatch && recentUpdatedAt) ? item.media : entries[0].media
             }, function (err, hashes) {
               //updated
             })
           }
         } else {
-          Hash.update({ uuid: item.uuid }, {
-            seeders: item.seeders,
-            leechers: item.leechers,
-            updatedAt: entries[0].updatedAt,
-            peersUpdatedAt: item.peersUpdatedAt,
-            updatedBy: item.updatedBy
-          }, function (err, hashes) {
-            //updated
-          })
+          console.log(item.uuid + " is already most updated.")
         }
       } else {
         delete item._id
