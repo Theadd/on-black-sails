2e756a67dbcdb7032e6786e406bbc8c55187c04c
Theadd <pantallazo@gmail.com>
2014-10-27 19:12:21 +0100

    added more info in agreements  page

--- config/http.js
+++ config/http.js
@@ -41,7 +41,7 @@ module.exports.http = {
 
   locals: {
     filters: {
-      formatBigNumber: function(num) {
+      formatBigNumber: function (num) {
         if (num >= 1000000000) {
           return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'G';
         }
@@ -52,10 +52,79 @@ module.exports.http = {
           return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
         }
         return num;
+      },
+
+      getAgreementFilterObject: function (name, param) {
+        var obj = {
+          display: 'Error',
+          description: 'Unknown',
+          icon: 'fa-exclamation-triangle'
+        };
+
+        switch (String(name).toLowerCase()) {
+          case 'indexed':
+            obj.description = 'Latest indexed torrents (w/o metadata).';
+            obj.display = 'Indexed';
+            obj.icon = 'fa-archive';
+            break;
+          case 'downloaded':
+            obj.description = 'Latest downloaded torrents (w/ metadata).';
+            obj.display = 'Downloaded';
+            obj.icon = 'fa-code';
+            break;
+          case 'media':
+            obj.description = 'Latest torrents with media info.';
+            obj.display = 'Media';
+            obj.icon = 'fa-film';
+            break;
+          case 'peers':
+            obj.description = 'Latest torrents with updated peers.';
+            obj.display = 'Peers';
+            obj.icon = 'fa-child';
+            break;
+        }
+
+        if (param || false) {
+          return obj[param];
+        } else {
+          return obj;
+        }
+      },
+
+      getAgreementFilterIcon: function (name) {
+        switch (String(name).toLowerCase()) {
+          case 'indexed': return 'fa-archive'; break;
+          case 'downloaded': return 'fa-code'; break;
+          case 'media': return 'fa-film'; break;
+          case 'peers': return 'fa-child'; break;
+        }
+
+        return 'fa-exclamation-triangle';
+      },
+
+      getAgreementFilterDisplay: function (name) {
+        switch (String(name).toLowerCase()) {
+          case 'indexed': return 'Indexed'; break;
+          case 'downloaded': return 'Downloaded'; break;
+          case 'media': return 'Media'; break;
+          case 'peers': return 'Peers'; break;
+        }
+
+        return 'fa-exclamation-triangle';
+      },
+
+      getAgreementFilterDescription: function (name) {
+        switch (String(name).toLowerCase()) {
+          case 'indexed': return 'Latest indexed torrents (w/o metadata).'; break;
+          case 'downloaded': return 'Latest downloaded torrents (w/ metadata).'; break;
+          case 'media': return 'Latest torrents with media info.'; break;
+          case 'peers': return 'Latest torrents with updated peers.'; break;
+        }
+
+        return 'fa-exclamation-triangle';
       }
     }
   },
-
   // The number of seconds to cache flat files on disk being served by
   // Express static middleware (by default, these files are in `.tmp/public`)
   //
